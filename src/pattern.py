b533c9df23c1aab17d2ef373484207def69b11f1
currocam <campuzanocurro@gmail.com>
2022-09-05 14:06:36 +0200

    refactor: better naming

--- src/pattern.py
+++ src/pattern.py
@@ -1,6 +1,6 @@
 
 # Print the pattern
 from itertools import chain
-concatenated = chain(range(1, 6), range(4, 0, -1))
-for i in concatenated:
+ranges = chain(range(1, 6), range(4, 0, -1))
+for i in ranges:
     print(" ".join(list("*"*i)))
