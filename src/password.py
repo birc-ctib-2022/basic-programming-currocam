0c327c09b04b219a5ce7ad9e9bb8d2ca05e350fc
currocam <campuzanocurro@gmail.com>
2022-09-05 12:53:59 +0200

    refactor: password

--- src/password.py
+++ src/password.py
@@ -8,14 +8,10 @@ password = sys.argv[1]
 it_has_lower, it_has_upper, it_has_number, it_has_special_char = False, False, False, False
 # Do all the requirement checks here.
 for c in password:
-    if (not it_has_lower) and (c.islower()):
-        it_has_lower = True
-    if (not it_has_upper) and (c.isupper()):
-        it_has_upper = True
-    if (not it_has_number) and (c.isnumeric()):
-        it_has_number = True
-    if (not it_has_special_char) and (c in ["$", "#", "@"]):
-        it_has_special_char = True
+    if not it_has_lower: it_has_lower = c.islower()
+    if not it_has_upper: it_has_upper = c.isupper()
+    if not it_has_number: it_has_number = c.isnumeric()
+    if not it_has_special_char: it_has_special_char = c in ["$", "#", "@"]
 
 is_valid = all([it_has_lower, it_has_upper, it_has_number, it_has_special_char])
 print(is_valid)
