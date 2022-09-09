00fef3a014f39925cd8109611ad9e84af67f9534
currocam <campuzanocurro@gmail.com>
2022-09-09 09:08:04 +0200

    refactor: more concise

--- src/print-1-10-growing.py
+++ src/print-1-10-growing.py
@@ -2,4 +2,4 @@
 # Print the numbers described in the exercise
 numbers = [str(n) for n in range(1, 11)]
 for i, v in enumerate(numbers):
-    print(" ".join(numbers[0:i+1]))
+    print(*numbers[0:i+1])
