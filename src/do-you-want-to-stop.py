b76addbd5c95bdc8ddc21cd06c3ca57920572ff8
currocam <campuzanocurro@gmail.com>
2022-09-09 08:39:36 +0200

    refactor: avoid repetition

--- src/do-you-want-to-stop.py
+++ src/do-you-want-to-stop.py
@@ -1,6 +1,6 @@
 
 # You should write a loop around this question, and keep asking until
 # the answer is "yes" (lower case).
-answer = input('Do you want to stop? ')
+answer = ""
 while answer != "yes":
     answer = input('Do you want to stop? ')
